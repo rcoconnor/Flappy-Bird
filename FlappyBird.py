42b3cc9fbba83c124177e8493aa11e4dbecce941
rcoconnor <33356261+rcoconnor@users.noreply.github.com>
2018-04-30 21:44:25 -0500

    added a background as well as character functions
    The player now has better controls as well as a roof and floor
    The background image has been added

--- FlappyBird.py
+++ FlappyBird.py
@@ -4,9 +4,14 @@ import os
 pygame.init()
 pygame.font.init()
 
+# colors 
 white = (255, 255, 255)
+blue = (0, 0, 200)
 
-screen = pygame.display.set_mode((360, 640))
+height = 640
+width = 360
+
+screen = pygame.display.set_mode((width, height))
 
 pygame.display.set_caption("Pickin Sticks")
 
@@ -23,38 +28,105 @@ def load_image(name, colorkey = None):
 		image.set_colorkey(colorkey, pygame.RLEACCEL)
 	return image
 
+
 class Player(pygame.sprite.Sprite): 
 	# This class will create a player class with
-	# 	-a sprite
-	# 	-height
+	# 	- a sprite image and rect
+	# 	- x and y poss
 
 	def __init__(self): 
+		
+		# sprite properties 
 		pygame.sprite.Sprite.__init__(self)
 		self.image = load_image("bird_sprite.jpg", -1)
 		self.image = pygame.transform.scale(self.image, (100, 100))
 		self.rect = self.image.get_rect()
 		self.ypos = 300
-		self.xpos = 200
+		self.xpos = 120
 		self.rect.topleft = (self.xpos, self.ypos)
+		
+		# physics properties
+		self.gravity = 0.5
+		self.lift = 20
+		self.velocity = 0
+		self.air_resistance = 0.95
+
+	def jump(self): 
+		self.velocity -= self.lift
+
+	def update(self): 
+		# this funciton upadates all the values of the player's bird
+		# check to make sure the bird isn't too low
+		
+		"""if self.rect.bottom > height: 
+			self.y = height
+			self.velocity = 0
+
+		# check to make sure the bird isn't too high
+		if self.rect.top < 0: 
+			self.y = 0
+			self.velocity = 0 """
+
+		# update the birds position
+		self.velocity += self.gravity
+		self.velocity *= self.air_resistance
+		newpos = self.rect.move(0, self.velocity)
+		self.rect = newpos
+
+		if self.rect.bottom > height: 
+			self.velocity *= -1
+
+		if self.rect.top < 0: 
+			newpos = self.rect.move(0, self.rect.top)
+			self.velocity *= -0.85
+
+
+class Background(pygame.sprite.Sprite): 
+	# This class creates a background object and loads the image into the game
+	# Image: Asian Mountain Forest BG by Crisisworks 
+	# URL: https://opengameart.org/content/asian-mountain-forest-bg
+	def __init__(self, imageFile, location): 
+		pygame.sprite.Sprite.__init__(self)
+		self.image = load_image(imageFile)
+		self.rect = self.image.get_rect()
+		self.rect.left, self.rect.top = location
+
 
 
 def game_loop(): 
 
-	screen.fill(white)
+	
+
+	screen.fill(blue)
+
 
 	player = Player()
+	BackGround = Background("imageBackground.png", [0,0])	
 
 	game_exit = False
-
 	allsprites = pygame.sprite.RenderPlain((player))
 
 
+	clock = pygame.time.Clock()
+
 	while not game_exit: 
+		# lock the game to 50 fps
+		clock.tick(50)
+
+		#Event Handling
 		for event in pygame.event.get(): 
+			# if the player is trying to quit
 			if event.type == pygame.QUIT: 
 				game_exit = True
+			# handle jumping 
+			if event.type == pygame.KEYDOWN: 
+				if event.key == pygame.K_SPACE: 
+					player.jump()
+
 
 		allsprites.update()
+		screen.fill((255, 255, 255))
+		screen.blit(BackGround.image, BackGround.rect)
 		allsprites.draw(screen)
 		pygame.display.update()
 
